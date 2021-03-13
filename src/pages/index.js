300edf59d67397484aa85f96ff2c909c779d3cb3
Niall Lewin <Lewin91@hotmail.com>
2021-03-13 21:49:20 +0000

    Added some hooks (TODO: convert to tsx properly)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,48 @@
 import * as React from "react"
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
 import { Link } from "gatsby"
 import { Layout } from './../components'
+import { useRemoveDupes, useGroupBy } from './../hooks'
 
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hello</h1>
-  </Layout>
-)
+const IndexPage = ({...props}) => {
+
+  const stories = get(props, "data.allContentfulComic.edges");
+  const storyTitles = useRemoveDupes(stories.map(({node}) => node.story.slug));
+  const groupedStories = useGroupBy(stories, (c) => c.node.story.slug);
+  debugger;
+
+
+    return(
+      <Layout>
+        <h1>Hello</h1>
+        {storyTitles.map(c => <h2>{c}</h2>)}
+      </Layout>
+    )
+}
 
 export default IndexPage
+
+export const query = graphql`
+query {
+    allContentfulComic {
+        edges {
+            node {
+                title
+                book
+                page
+                story {
+                    title
+                    slug
+                }
+                node_locale
+                image {
+                    fluid {
+                        ...GatsbyContentfulFluid_withWebp
+                    }
+                }
+            }
+        }
+    }
+}`
