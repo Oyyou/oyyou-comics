32c09a93461026c438dc292b09ea856017955deb
Niall Lewin <Lewin91@hotmail.com>
2021-03-31 11:50:32 +0100

    Changed from G to UA

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-LXDG167DC0",
+        trackingId: "UA-193469561-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
