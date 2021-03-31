010ad78e86e487849a404bdc3694655096fd08b1
Niall Lewin <Lewin91@hotmail.com>
2021-03-31 15:27:40 +0100

    Removed duplication

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,8 +65,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "OyyouComics",
-        short_name: "OyyouComics",
+        name: "Oyyou Comics",
+        short_name: "Oyyou Comics",
         start_url: "/",
         background_color: "#202124",
         theme_color: "#d4d4d4",
@@ -112,18 +112,6 @@ module.exports = {
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/Logo.png`, // This path is relative to the root of the site.
-      },
-    },
     {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
