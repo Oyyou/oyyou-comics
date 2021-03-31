6195eb703515b45ecc89711af1c3559725968783
Niall Lewin <Lewin91@hotmail.com>
2021-03-31 14:41:19 +0100

    PWA stuff?

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,24 @@ module.exports = {
         cookieDomain: "oyyou-comics.com",
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "OyyouComics",
+        short_name: "OyyouComics",
+        start_url: "/",
+        background_color: "#202124",
+        theme_color: "#d4d4d4",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/Logo.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
@@ -109,6 +127,6 @@ module.exports = {
     `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
